36c96ff4bc730fb567995a23a2b951c34b7cf4c0
VireshShah17 <88429687+VireshShah17@users.noreply.github.com>
2023-05-05 23:21:14 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -44,15 +44,4 @@ class Graph:
             return True
         return False
 
-my_graph = Graph()
-my_graph.add_vertex('A')
-my_graph.add_vertex('B')
-my_graph.add_vertex('C')
-my_graph.add_vertex('D')
-my_graph.add_edge('A', 'B')
-my_graph.add_edge('A', 'C')
-my_graph.add_edge('A', 'D')
-my_graph.add_edge('B', 'D')
-my_graph.add_edge('C', 'D')
-my_graph.remove_vertex('D')
-my_graph.print_graph()
+
